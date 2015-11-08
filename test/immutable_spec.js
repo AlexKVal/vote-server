4b806496e7b95fdc6ab9e4f069e599442423ad02
AlexKVal <alexkval@gmail.com>
2015-11-08 08:43:31 +0200

    [added] test case for learning a more elaborate state type

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 describe('immutability', () => {
   describe('a number', () => {
@@ -29,6 +29,31 @@ describe('immutability', () => {
       expect(nextState).to.equal(List.of('Garry', '28hrs', 'Sun'));
 
       expect(state).to.equal(List.of('Garry', '28hrs'));
+    })
+  })
+
+  describe('a tree', () => {
+    function addMovie(currentState, movie) {
+      return currentState.set(
+        'movies',
+        currentState.get('movies').push(movie)
+      )
+    }
+
+    it('is immutable', () => {
+      let state = Map({
+        movies: List.of('Garry', '28hrs')
+      });
+      let nextState = addMovie(state, 'Sun');
+
+      expect(nextState).to.equal(Map({
+        movies: List.of('Garry', '28hrs', 'Sun')
+      }));
+
+      // assert that 'state' has not been mutated
+      expect(state).to.equal(Map({
+        movies: List.of('Garry', '28hrs')
+      }))
     })
   })
 })
