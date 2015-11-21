dce5b6f74ad51ff0c0924d98257bb59ee5c3b965
AlexKVal <alexkval@gmail.com>
2015-11-21 14:19:13 +0200

    move out the general code to 'serializeState()' function

--- src/server.js
+++ src/server.js
@@ -1,17 +1,21 @@
 import Server from 'socket.io';
 
+function serializeState(store) {
+  return store.getState().toJS();
+}
+
 export default function startServer(store) {
   const io = new Server().attach(8090);
 
   store.subscribe(() => {
     console.log('a change has occured in Store');
 
-    io.emit('state', store.getState().toJS()
+    io.emit('state', serializeState(store));
   }));
 
   io.on('connection', (socket) => {
     console.log('a client has been connected');
 
-    socket.emit('state', store.getState().toJS());
+    socket.emit('state', serializeState(store));
   });
 }
