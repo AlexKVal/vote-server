50328d52318623fbfed0a25bd1ae2754473a5b88
AlexKVal <alexkval@gmail.com>
2015-11-21 14:16:49 +0200

    [added] send 'state' to a client on connection event

--- src/server.js
+++ src/server.js
@@ -3,11 +3,15 @@ import Server from 'socket.io';
 export default function startServer(store) {
   const io = new Server().attach(8090);
 
-  store.subscribe(
-    () => {
-      console.log('a change has occured in Store');
+  store.subscribe(() => {
+    console.log('a change has occured in Store');
 
-      io.emit('state', store.getState().toJS()
-    })
-  );
+    io.emit('state', store.getState().toJS()
+  }));
+
+  io.on('connection', (socket) => {
+    console.log('a client has been connected');
+
+    socket.emit('state', store.getState().toJS());
+  });
 }
